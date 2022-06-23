84758313ea3bce4d4958c38c5c02a4093c2bf8ff
Neven <neven.boisson@hotmail.fr>
2022-06-23 16:19:17 +0200

    deleted duplicate file

--- agios.js
+++ agios.js
@@ -3,13 +3,13 @@ let day = 0;
 let agios = 0;
 
 do {
-overdraft = prompt ('Demander le montant du découvert autorisé');
-day = prompt ('Demandez le nombre de jours de découvert');
+    overdraft = prompt('Demander le montant du découvert autorisé');
+    day = prompt('Demandez le nombre de jours de découvert');
 
 } while (overdraft > 2000 || overdraft < 100 || day < 1 || day > 365);
 
-agios = (parseInt(overdraft) * parseInt(day) * 0.1) / 365
-agios = agios.toFixed(2)
-alert ('Montant des agios '+ agios);
+agios = (parseInt(overdraft) * parseInt(day) * 0.1) / 365;
+agios = agios.toFixed(2);
+alert('Montant des agios ' + agios);
 
-console.log ('Le nombre de jours convient à la condition comprise entre 1 et 365');
+console.log('Le nombre de jours convient à la condition comprise entre 1 et 365');
--- index.js
+++ index.js
@@ -4,14 +4,14 @@ let sold;
 
 alert('Bienvenue chez GTM Bank');
 isOverdraft = prompt("Voulez-vous avoir un décourvert ? (y ou Y pour oui, n'importe quel caractère pour non");
-if(isOverdraft=='y' || isOverdraft=='Y'){
-    do{
+if (isOverdraft == 'y' || isOverdraft == 'Y') {
+    do {
         overdraft = parseInt(prompt("Saisissez le montant du découvert entre 100€ et 2000€"));
-    }while(overdraft < 100 || overdraft > 2000);
+    } while (overdraft < 100 || overdraft > 2000);
     document.write(`Vous avez choisit d\'avoir un découvert de ${overdraft}€<br>`);
 }
-do{
+do {
     sold = parseInt(prompt("Saisissez le dépot (montant d'ouvrture de compte minimum de 500€)"));
-}while(sold < 500);
+} while (sold < 500);
 document.write(`Solde (€): ${sold}€<br>`);
 document.write(`Découvert (€): ${overdraft}€<br>`);
