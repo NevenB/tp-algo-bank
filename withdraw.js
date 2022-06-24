d43170b31b0bce9996d96c466a32605ccd00fbd8
Neven <neven.boisson@hotmail.fr>
2022-06-24 15:53:58 +0200

    Cleaning

---
+++ withdraw.js
@@ -0,0 +1,28 @@
+function withdraw() {
+    let sold, overdraft, withdraw, stopwithdraw;
+    alert('Bienvenue chez GTM Bank');
+    do {
+        overdraft = parseInt(prompt('Saisissez le montant de votre décovert (0 si vous n\'avez pas droit au découvert)'));
+        if (overdraft == 0) {
+            alert("Vous n'avez pas droit au découvert");
+            break;
+        }
+    } while (overdraft < 100 | overdraft > 2000);
+
+    if (overdraft != 0) {
+        alert('Vous avez droit au découvert');
+    }
+    sold = parseInt(prompt('Saisissez le montant de votre solde'));
+
+    withdraw = parseInt(prompt('Saississez le montant du retrait')); 
+    
+    if (withdraw > sold + overdraft) {
+        alert("Solde insuffisant");
+    } else {
+        alert("Retrait autorisé");
+        sold = sold - withdraw;
+        alert("Le montant de votre solde est donc de " + sold + "€");
+    }
+}
+
+withdraw();
