a44c0687f34bd7048fe6807263666325c18a8807
Neven <neven.boisson@hotmail.fr>
2022-06-27 17:22:47 +0200

    Js - open account

--- new_account.js
+++ new_account.js
@@ -1,17 +1,33 @@
-function openAccount() {
-    alert('Bienvenue chez GTM Bank');
-    isOverdraft = prompt("Voulez-vous avoir un décourvert ? (y ou Y pour oui, n'importe quel caractère pour non");
-    if (isOverdraft == 'y' || isOverdraft == 'Y') {
-        do {
-            overdraft = parseInt(prompt("Saisissez le montant du découvert entre 100€ et 2000€"));
-        } while (overdraft < 100 || overdraft > 2000);
-        document.write(`Vous avez choisit d\'avoir un découvert de ${overdraft}€<br>`);
-    }
-    do {
-        sold = parseInt(prompt("Saisissez le dépot (montant d'ouvrture de compte minimum de 500€)"));
-    } while (sold < 500);
-    document.write(`Solde (€): ${sold}€<br>`);
-    document.write(`Découvert (€): ${overdraft}€<br>`);
-}
+const radioYes = document.getElementById('yes');
+const radioNo = document.getElementById('no');
+const boxOverdraft = document.getElementById('boxOverdraft');
 
-openAccount();
+radioYes.addEventListener('click', function () {
+    boxOverdraft.style.visibility = 'visible';
+});
+
+radioNo.addEventListener('click', function () {
+    boxOverdraft.style.visibility = 'hidden';
+});
+
+const boxError = document.getElementById('error');
+
+const btn = document.getElementById('open_account');
+btn.addEventListener('click', function () {
+    const inputOverdraft = document.getElementById('overdraft');
+    const inputSold = document.getElementById('sold');
+
+    if(radioYes.value){
+        if (inputOverdraft.value < 100 || inputOverdraft.value > 2000){
+            boxError.innerHTML = 'Overdraft must be between 100 and 2000 deposit must be greater or equals to 500';
+        }else{
+            sessionStorage.setItem('overdraft', inputOverdraft.value);
+        }
+    }
+    if (inputSold.value < 500) {
+        boxError.innerHTML = 'Overdraft must be between 100 and 2000 deposit must be greater or equals to 500';
+    }else{
+        sessionStorage.setItem('sold', inputSold.value); 
+        location.href = 'withdraw.html';
+    }
+});
