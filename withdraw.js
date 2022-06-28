c88f29c969c0efe17d3810ecfcc2387754d6ca0f
Neven <neven.boisson@hotmail.fr>
2022-06-28 15:35:03 +0200

    Formatage + Comments

--- withdraw.js
+++ withdraw.js
@@ -18,37 +18,34 @@ document.getElementById("overdraft").innerHTML = compte1.overdraft;
 function withdraw(e) {
     e.preventDefault();
 
+    // On récupère la valeur que l'utilisateur à rentrée
     const inputElement = document.getElementById("number");
-    let number = parseFloat(inputElement.value);
+
+    let number = Number(inputElement.value);
 
     sessionStorage.setItem("number", number);
     const errorSold = document.getElementById("errorSold");
- 
+
     const isPossible = Number(compte1.sold) + Number(compte1.overdraft);
-    console.log("Is possible : ", isPossible);
     if (number > isPossible) {
         errorSold.innerHTML = "Retrait refusé, pour solde insuffisant";
-        console.log(`${number} > ${compte1.sold} + ${compte1.overdraft}`);
-        console.log(number > compte1.sold + compte1.overdraft);
     } else {
         errorSold.innerHTML = "Retrait autorisé";
+
         const soldElement = document.getElementById("sold");
         // Je crée une variable pour mettre à jour le sold
-        const newSold =  compte1.doWithdraw(number);
+        const newSold = compte1.doWithdraw(number);
         // Affiche la nouvelle valeur du solde
-        soldElement.textContent = newSold
+        soldElement.textContent = newSold;
         // Permet de stocker la valeur dans le session storage
         sessionStorage.setItem("sold", newSold);
     }
-    inputElement.value="";
+    inputElement.value = "";
 }
-
-
 
 const btnOpen = document.getElementById("open");
 const btnAggio = document.getElementById("aggio");
 const btnWithdraw = document.getElementById("withdraw");
-
 
 btnOpen.addEventListener("click", function (e) {
     e.preventDefault();
