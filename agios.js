382685fe87aab058eedad7d7bb0b0eb8b43a976d
Fabricedeubras <f.deubras@gmail.com>
2022-06-28 11:46:21 +0200

    add redirection

--- agios.js
+++ agios.js
@@ -9,3 +9,17 @@ function getDay(e) {
     sessionStorage.setItem("day", day)
     console.log(day)
 }
+
+const btnOpen = document.getElementById("open");
+const btnAggio = document.getElementById("aggio");
+const btnWithdraw = document.getElementById("withdraw");
+
+btnOpen.addEventListener("click", function(){
+    location.href = 'open.html';
+})
+btnAggio.addEventListener("click", function(){
+    location.href = 'agios.html';
+})
+btnWithdraw.addEventListener("click", function(){
+    location.href = 'withdraw.html';
+} )
