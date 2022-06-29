e817620849b615465322f3d27304f7bd6c5c6270
Neven <neven.boisson@hotmail.fr>
2022-06-29 10:02:44 +0200

    Bug correction on sessionStorage data + some comments

--- open.js
+++ open.js
@@ -4,10 +4,12 @@ const boxOverdraft = document.getElementById('boxOverdraft');
 
 radioYes.addEventListener('click', function () {
     boxOverdraft.style.visibility = 'visible';
+    document.getElementById('overdraft').value = '';
 });
 
 radioNo.addEventListener('click', function () {
     boxOverdraft.style.visibility = 'hidden';
+    document.getElementById('overdraft').value = '';
 });
 
 const boxErrorOverdraft = document.getElementById('errorOverdraft');
@@ -15,37 +17,41 @@ const boxErrorerrorDeposit = document.getElementById('errorDeposit');
 
 const btn = document.getElementById('open_account');
 btn.addEventListener('click', function () {
+    //On récupère les informations entrées par l'utilisateur 
     const inputOverdraft = document.getElementById('overdraft');
     const inputSold = document.getElementById('sold');
 
+    //On remet à zéro les messages d'erreurs
     boxErrorerrorDeposit.innerHTML = '';
     boxErrorOverdraft.innerHTML = '';
 
 
-    if(radioYes.value){
-        if (inputOverdraft.value < 100 || inputOverdraft.value > 2000){
-            boxErrorOverdraft.innerHTML = 'Overdraft must be between 100 and 2000';
-        }else{
-            sessionStorage.setItem('overdraft', inputOverdraft.value);
+    if (radioNo.checked) { // Si l'utilisateur ne veux pas de découvert
+        sessionStorage.setItem('overdraft', 0); // On stock la valeur 0 dans le session storage
+    } else { // Si l'utilisateur veux un découvert
+        if (inputOverdraft.value < 100 || inputOverdraft.value > 2000) { // On vérifie que la valeure rentrée entre dans le bon interval 
+            boxErrorOverdraft.innerHTML = 'Overdraft must be between 100 and 2000'; // On affiche le message d'erreur correspondant
+        } else {
+            sessionStorage.setItem('overdraft', inputOverdraft.value); // Si tout est bon, on stocke la valeur dans le session storage
         }
     }
     if (inputSold.value < 500) {
         boxErrorerrorDeposit.innerHTML = 'Deposit must be greater or equals to 500';
-    }else{
-        sessionStorage.setItem('sold', inputSold.value); 
+    } else {
+        sessionStorage.setItem('sold', inputSold.value);
         location.href = 'withdraw.html';
     }
-
     inputOverdraft.value = '';
     inputSold.value = '';
 });
 
-document.getElementById('open').addEventListener('click', function(){
+
+document.getElementById('open').addEventListener('click', function () {
     location.href = 'open.html';
 });
-document.getElementById('aggio').addEventListener('click',function(){
+document.getElementById('aggio').addEventListener('click', function () {
     location.href = 'agios.html';
 });
-document.getElementById('withdraw').addEventListener('click',function(){
+document.getElementById('withdraw').addEventListener('click', function () {
     location.href = 'withdraw.html';
 });
